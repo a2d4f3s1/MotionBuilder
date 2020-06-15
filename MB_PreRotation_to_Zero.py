1f46af1b42076cb195b9d3ff34400925e5d84262
a2d4f3s1 <a2d4f3s1@gmail.com>
2020-06-16 04:59:18 +0900

    v2

--- MB_PreRotation_to_Zero.py
+++ MB_PreRotation_to_Zero.py
@@ -1,9 +1,19 @@
-from pyfbsdk import FBConstraintManager, FBModelList, FBGetSelectedModels
+# *- coding: utf-8 -*-　#UTF8宣言使えば日本語使える
+from pyfbsdk import FBConstraintManager, FBObjectList, FBGetSelectedObjects
 
-Models = FBModelList()
-FBGetSelectedModels(Models)
+Objects = FBObjectList()
+FBGetSelectedObjects(Objects)
 
-for Model in Models:
-    print(Model.Name)
+for Object in Objects:
+    print("Name = " + Object.Name)  #ObjectName
+    RotDOF = Object.PropertyList.Find("RotationActive").Data  #Enable Rotation DOF
+    print("RotDOF = " + str(RotDOF))
+    LclRot=Object.PropertyList.Find("Lcl Rotation").Data #Lcl Rotation
+    print("Lcl Rotation = " + str(LclRot))
+    PreRot=Object.PropertyList.Find("PreRotation").Data #PreRotation
+    print("PreRotatio = " + str(PreRot))
+    
+    AAA = LclRot + PreRot
+    print (AAA)
 
-print("END")
+print("終わり")

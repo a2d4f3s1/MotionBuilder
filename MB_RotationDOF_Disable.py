2e835027ec6594d3ffbf3ef37d8df07a8e44714d
a2d4f3s1 <a2d4f3s1@gmail.com>
2020-06-16 14:09:17 +0900

    V5？

--- MB_RotationDOF_Disable.py
+++ MB_RotationDOF_Disable.py
@@ -3,8 +3,9 @@ from pyfbsdk import FBConstraintManager, FBModelList, FBGetSelectedModels
 
 Models = FBModelList()
 FBGetSelectedModels(Models)
-
+OtherThanEulerXYZs = FBModelList()
 count = 0
+
 print("----- DOF Disable 処理開始 -----\n")
 
 for Model in Models:
@@ -13,16 +14,21 @@ for Model in Models:
     if Model.Rotation.GetAnimationNode() is None:  # アニメーションが無い
         RotDOF = Model.PropertyList.Find("RotationActive")  # Enable Rotation DOF
         if RotDOF.Data is True:
-            LclRot = Model.PropertyList.Find("Lcl Rotation")  # Lcl Rotation
-            print("Lcl Rotation = " + str(LclRot.Data))
-            PreRot = Model.PropertyList.Find("PreRotation")  # PreRotation
-            print("Pre Rotation = " + str(PreRot.Data))
-            print("LclRotationを更新、DOFを無効化します")
-            AddRot = LclRot.Data + PreRot.Data  # ただの足し算、、、バグったらたぶんここ
-            LclRot.Data = AddRot
-            print("Lcl Rotation = " + str(LclRot.Data))
-            RotDOF.Data = False
-            print("Enable Rotation DOF = " + str(RotDOF.Data))
+            RotOrder = Model.PropertyList.Find("RotationOrder")  # Enable Rotation DOF
+            if RotOrder.Data == 0:
+                LclRot = Model.PropertyList.Find("Lcl Rotation")  # Lcl Rotation
+                print("Lcl Rotation = " + str(LclRot.Data))
+                PreRot = Model.PropertyList.Find("PreRotation")  # PreRotation
+                print("Pre Rotation = " + str(PreRot.Data))
+                print("LclRotationを更新、DOFを無効化します")
+                AddRot = LclRot.Data + PreRot.Data  # ただの足し算、、、バグったらたぶんここ
+                LclRot.Data = AddRot
+                print("Lcl Rotation = " + str(LclRot.Data))
+                RotDOF.Data = False
+                print("Enable Rotation DOF = " + str(RotDOF.Data))
+            else:
+                print("RotationOrderがEuler XYZではありません")
+                OtherThanEulerXYZs.append(Model)
         else:
             print("DOFは無効です")
     else:
@@ -30,3 +36,10 @@ for Model in Models:
     count = count + 1
 
 print("----- DOF Disable 処理完了 -----")
+for i in Models:  # 選択解除
+    i.Selected = False
+Models.Selected = False
+print ("EulerXYZ以外を選択します")  # EulerEYX以外を選択
+for i in OtherThanEulerXYZs:
+    print (i.Name)
+    i.Selected = True
